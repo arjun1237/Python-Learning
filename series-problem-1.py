be5eed309bd6706a55031a66a95cb03075e30ee1
Arjun K Prasad <arjun.blr1237@gmail.com>
2019-09-06 02:21:10 +0530

    Update series-problem-1.py

--- series-problem-1.py
+++ series-problem-1.py
@@ -1,3 +1,8 @@
+# Write Python code to find if ALL the numbers in a given list of 
+# integers are PART of the series defined by the following. 
+# f(0) = 0, f(1) = 1, f(n) = 2*f(n-1) - 2*f(n-2) for all n > 1
+
+
 m = {0: 0, 1: 1}
 
 
