d638e12a860ac5b3c712435905a4e393054510f3
Arjun K Prasad <arjun.blr1237@gmail.com>
2019-09-14 08:07:49 +0530

    Create insertion-sort.py

---
+++ insertion-sort.py
@@ -0,0 +1,16 @@
+def insertion(list1):
+    list_len = len(list1)
+    for i in range(list_len):
+        small = i
+        swap = False
+        for j in range(i+1, list_len):
+            if list1[small] > list1[j]:
+                small = j
+                swap = True
+        if swap:
+            list1[i], list1[small] = list1[small], list1[i]
+
+    return list1
+
+
+print(insertion([64, 34, 25, -12, 22, 11, -90]))  # test case
