181dd0f9e69915bd392d3518c4ee2a8ec6538c94
Arjun K Prasad <arjun.blr1237@gmail.com>
2020-03-19 15:49:02 +0530

    Update matching parenthesis.py

--- matching parenthesis.py
+++ matching parenthesis.py
@@ -1,35 +1,70 @@
+# much more efficient way:
+# -------------------------------------------
+    
+parenthesis = {
+    '[': ']',
+    '{': '}',
+    '(': ')'
+}
 
-s = [
-    ['{', '}'],
-    ['[', ']'],
-    ['(', ')']
-]
 
-
-def matchParen(x):
-    b = []
-    for i in x:
-        if open_brack(i):
-            b.append(i)
+def check_paren(word):
+    list1 = []
+    keys = parenthesis.keys()
+    for i in word:
+        if i in keys:
+            list1.append(i)
+        elif parenthesis[list1[-1]] == i:
+            list1.pop()
         else:
-            if len(s) == 0:
-                return False
-            elif not match(b.pop(), i):
-                return False
-    return len(b) == 0
+            return False
+    return "Balanced" if not list1 else "Not balanced"
 
 
-def open_brack(brack):
-    for i in s:
-        if i[0] == brack:
-            return True
-    return False
+print(check_paren("{[]{()}}"))
 
 
-def match(brack1, brack2):
-    for i in s:
-        if i[1] == brack2 and i[0] == brack1:
-            return True
-    return False
+# old one
+# -----------------------------------------------------------
 
-print(matchParen("[(])")) # test case - returns false
+# s = [
+#     ['{', '}'],
+#     ['[', ']'],
+#     ['(', ')']
+# ]
+
+
+# def matchParen(x):
+#     b = []
+#     for i in x:
+#         if open_brack(i):
+#             b.append(i)
+#         else:
+#             if len(s) == 0:
+#                 return False
+#             elif not match(b.pop(), i):
+#                 return False
+#     return len(b) == 0
+
+
+# def open_brack(brack):
+#     for i in s:
+#         if i[0] == brack:
+#             return True
+#     return False
+
+
+# def match(brack1, brack2):
+#     for i in s:
+#         if i[1] == brack2 and i[0] == brack1:
+#             return True
+#     return False
+
+
+# print(matchParen("[(])")) # test case - returns false
+
+
+
+
+
+
