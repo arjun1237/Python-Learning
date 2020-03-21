04966fa244dac1e6fe4fd38269248172a521255d
Arjun K Prasad <arjun.blr1237@gmail.com>
2020-03-21 15:10:12 +0530

    Create traverse folder.py
    print all files from traversing a given path

---
+++ traverse folder.py
@@ -0,0 +1,10 @@
+def traverse_file(path_name):
+    for file in listdir(path_name):
+        file_fold = join(path_name, file)
+        if isdir(file_fold):
+            traverse_file(file_fold)
+        elif isfile(file_fold):
+            print(path_name + '\\' + file)
+
+
+traverse_file(path_n)
