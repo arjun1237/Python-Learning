bfc228b46ac8279208dd4b997e187a9ba8298d8e
Arjun K Prasad <arjun.blr1237@gmail.com>
2020-03-21 15:12:02 +0530

    Update traverse folder.py

--- traverse folder.py
+++ traverse folder.py
@@ -1,3 +1,5 @@
+# traversing a path and finding all files in it without using os.walk
+
 def traverse_file(path_name):
     for file in listdir(path_name):
         file_fold = join(path_name, file)
