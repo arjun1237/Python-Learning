bf5ac86f042823dba2b7deab25bbe1c63d999947
Arjun K Prasad <arjun.blr1237@gmail.com>
2020-04-02 14:24:44 +0530

    Update prime_nums.py

--- prime_nums.py
+++ prime_nums.py
@@ -1,3 +1,3 @@
-# prints prime number in one line
+# prints prime numbers between 1 and 150(included) in one line
 
 print([x for x in range(1, 150) if not any([x % y == 0 for y in range(2, int(x/2)+1)]) and not x == 1])
